a47859879964bb76692017f0f5f17cccb6e02aad
Владимир <100162794+Vladimir-Korolev555@users.noreply.github.com>
2023-09-28 21:42:28 +1100

    HomeWork completed!

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,89 @@
 public class Main {
     public static void main(String[] args) {
-        int salary = 65535;
-        int total = 0;
-        int i = 0;
-        for (; total < 1_000_000; i++) {
-            total = total + total / 100;
-            total = total + salary;
-            if (i % 5 == 0) {
-                System.out.println("Месяц " + i + " итого " + total);
-            }
+        //Task 1
+        int contributions = 15_000;
+        int totalContributions = 0;
+        int month = 0;
+        while (totalContributions <= 2_459_000) {
+            totalContributions = totalContributions + totalContributions / 100;
+            totalContributions = totalContributions + contributions;
+            month = month + 1;
+            System.out.println("Месяц " + month + ", сумма накоплений составляет " + totalContributions + " рублей");
         }
-        System.out.println(total);
+        System.out.println("Итого, сумма накоплений составила " + totalContributions + " рублей, понадобилось " + month + " месяцев");
 
-        int start = 5;
+        //Task 2
+        int start = 10;
         while (start > 0) {
-            System.out.println(start);
+            System.out.print(start + " ");
             start = start - 1;
         }
+        System.out.println();
+        int finish = 1;
+        while (finish <= 10) {
+            System.out.print(finish + " ");
+            finish = finish + 1;
+        }
+        System.out.println();
 
-        int days = 10; // изначальное количество дней в Париже
-
-        if (days == 0) {
-            System.out.println("Желаем приятной поездки!");
-        } else {
-            while (days > 0) {
-                days = days - 1;
-                System.out.println("До конца поездки осталось " + days + " дней.");
-            }
-            System.out.println("Путешествие окончено. Пора возвращаться домой.");
+        //Task 3
+        int populationsY = 12_000_000;
+        int birthRate = 17;
+        int mortality = 8;
+        int year = 0;
+        while (year < 10) {
+            populationsY = populationsY + (birthRate - mortality) * 1000;
+            year = year + 1;
+            System.out.println("Год " + year + " , численность населения составляет " + populationsY + " человек.");
         }
 
-        int fight = 5;
-        do {System.out.println (fight);
-            fight = fight - 1;
-        }
-        while (fight > 0);
-
-        int x = 8;
-        do {
-            System.out.println(x);
-            i++;
-        } while (x < 7);
-
-        // Объявляем переменную dayNumber, она равна номеру дня в месяце
-        for (int dayNumber = 1; dayNumber <= 31; dayNumber++) {
-            // Каждый день проверяем, является ли он третьим (выходным)
-            if (dayNumber % 3 == 0) {
-                // Выводим сообщение
-                System.out.println("Сегодня отдыхаем!");
-                // Ждем следующий день
-                continue;
+        //Task 4 and 5
+        int totalContribution = 15_000;
+        int monthVasilyLife = 0;
+        while (totalContribution <= 12_000_000) {
+            totalContribution = totalContribution + ((totalContribution * 7) / 100);
+            monthVasilyLife = monthVasilyLife + 1;
+            if (monthVasilyLife % 6 == 0) {
+                System.out.println("Месяц " + monthVasilyLife + " , накопления " + totalContribution + " рублей.");
             }
-            System.out.println("Сегодня рабочий день");
-            // Если день не выходной, выводим сообщение
+        }
+        System.out.println("Итого, за " + monthVasilyLife + " месяцев Василий накопил " + totalContribution + " рублей.");
+
+        //Task 6
+        int vasilyContribution = 15_000;
+        int monthLifeVasily = 0;
+        while (monthLifeVasily <= 9 * 12) {
+            vasilyContribution = vasilyContribution + ((vasilyContribution * 7) / 100);
+            monthLifeVasily = monthLifeVasily + 1;
+            if (monthLifeVasily % 6 == 0) {
+                System.out.println("За " + monthLifeVasily + " месяцев, Василий накопил " + vasilyContribution + " рублей.");
+            }
+        }
+
+        //Task 7
+        int day = 1;
+        int monthCode = 6; //1 — январь, октябрь; 2 — май; 3 — август; 4 — февраль, март, ноябрь; 5 — июнь; 6 — декабрь, сентябрь; 0 — апрель, июль.
+        int yearNow = 2023;
+        int yearCode;
+        int dayOfWeek;
+        while (day < 31) {
+            yearCode = (6 + yearNow - 2000 + (yearNow - 2000) / 4) % 7;
+            dayOfWeek = (day + monthCode + yearCode) % 7;
+            day = day + 1;
+            {
+                if (dayOfWeek == 5) {
+                    System.out.println("Сегодня " + day + " -е число, пятница. Пора готовить отчет!");
+                }
+            }
+        }
+
+        //Task 8
+        int yearFlyComet = 1817;
+        while (yearFlyComet < 2060) {
+            yearFlyComet = yearFlyComet + 79;
+            if (yearFlyComet > 1850 && yearFlyComet < 2100) {
+                System.out.println("В " + yearFlyComet + " году комета пролетала (-ет).");
+            }
         }
     }
 }
