447ffd3d628d5b96f752a674ad0931fc3a2a86f2
Владимир <100162794+Vladimir-Korolev555@users.noreply.github.com>
2023-10-01 20:51:28 +1100

    Homework has been finalized!

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,12 @@ public class Main {
     public static void main(String[] args) {
         //Task 1
         int contributions = 15_000;
+        int needContributions = 2_459_000;
         int totalContributions = 0;
         int month = 0;
-        while (totalContributions <= 2_459_000) {
-            totalContributions = totalContributions + totalContributions / 100;
-            totalContributions = totalContributions + contributions;
+        while (totalContributions <= needContributions) {
+            totalContributions += totalContributions / 100;
+            totalContributions += contributions;
             month = month + 1;
             System.out.println("Месяц " + month + ", сумма накоплений составляет " + totalContributions + " рублей");
         }
@@ -32,17 +33,20 @@ public class Main {
         int mortality = 8;
         int year = 0;
         while (year < 10) {
-            populationsY = populationsY + (birthRate - mortality) * 1000;
-            year = year + 1;
+            populationsY += populationsY * (birthRate - mortality) / 1000;
+            year++;
             System.out.println("Год " + year + " , численность населения составляет " + populationsY + " человек.");
         }
 
         //Task 4 and 5
         int totalContribution = 15_000;
         int monthVasilyLife = 0;
-        while (totalContribution <= 12_000_000) {
-            totalContribution = totalContribution + ((totalContribution * 7) / 100);
-            monthVasilyLife = monthVasilyLife + 1;
+        int needContribution = 12_000_000;
+        int percentage;
+        while (totalContribution <= needContribution) {
+            percentage = (totalContribution * 7) / 100;
+            totalContribution += percentage;
+            monthVasilyLife++;
             if (monthVasilyLife % 6 == 0) {
                 System.out.println("Месяц " + monthVasilyLife + " , накопления " + totalContribution + " рублей.");
             }
@@ -52,15 +56,18 @@ public class Main {
         //Task 6
         int vasilyContribution = 15_000;
         int monthLifeVasily = 0;
-        while (monthLifeVasily <= 9 * 12) {
-            vasilyContribution = vasilyContribution + ((vasilyContribution * 7) / 100);
-            monthLifeVasily = monthLifeVasily + 1;
+        int estimatedTime = 9 * 12;
+        while (monthLifeVasily <= estimatedTime) {
+            percentage = (vasilyContribution * 7) / 100;
+            vasilyContribution += percentage;
+            monthLifeVasily++;
             if (monthLifeVasily % 6 == 0) {
                 System.out.println("За " + monthLifeVasily + " месяцев, Василий накопил " + vasilyContribution + " рублей.");
             }
         }
 
-        //Task 7
+        /*
+        //Task 7 (Мое решение задачи, позволяющее находить реальные пятницы в месяце)
         int day = 1;
         int monthCode = 6; //1 — январь, октябрь; 2 — май; 3 — август; 4 — февраль, март, ноябрь; 5 — июнь; 6 — декабрь, сентябрь; 0 — апрель, июль.
         int yearNow = 2023;
@@ -69,20 +76,38 @@ public class Main {
         while (day < 31) {
             yearCode = (6 + yearNow - 2000 + (yearNow - 2000) / 4) % 7;
             dayOfWeek = (day + monthCode + yearCode) % 7;
-            day = day + 1;
+            day++;
             {
                 if (dayOfWeek == 5) {
                     System.out.println("Сегодня " + day + " -е число, пятница. Пора готовить отчет!");
                 }
             }
         }
+         */
 
-        //Task 8
+        //Task 7 (Решение для условного месяца)
+        int firstfriday = 1;
+        for (int currentFriday = firstfriday; currentFriday <= 31; currentFriday += 7) {
+            System.out.println("Сегодня пятница " + currentFriday + " -е число, пятница. Пора готовить отчет!");
+        }
+
+        /*
+        //Task 8 (Старое решение)
         int yearFlyComet = 1817;
         while (yearFlyComet < 2060) {
             yearFlyComet = yearFlyComet + 79;
             if (yearFlyComet > 1850 && yearFlyComet < 2100) {
                 System.out.println("В " + yearFlyComet + " году комета пролетала (-ет).");
+            }
+         */
+
+        //Task 8 (Новое решение)
+        int currentYear = 2023;
+        int beggining = currentYear - 200;
+        int ending = currentYear + 100;
+        for (int flyYear = beggining; flyYear < ending; flyYear++) {
+            if (flyYear % 79 == 0) {
+                System.out.println("В " + flyYear + " году комета пролетала (-ет).");
             }
         }
     }
